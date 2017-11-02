1f5e70e534ef73129bfbd29d7d4ef1d6f92814ff
A L I <aliseylaneh@gmail.com>
2017-11-02 16:22:46 +0330

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,27 @@
+package ir.javacup.paint;
+
+
+public class Shape {
+	Color color;
+	Pattern pattern;
+
+	public Shape(Color c, Pattern p) {
+		this.color = c;
+		this.pattern = p;
+	}
+
+}
+
+enum Color {
+	BLUE, GREEN, RED
+}
+
+enum Pattern {
+	DOTTED, STRIPED, ZIGZAG
+}
+
+enum Radius{
+
+10,20,30
+
+}
